fc0e16ca39a3fbbd925bbd1ed72c5c31c90ec0c5
RobertTong176 <tonghuy176@gmail.com>
2024-09-12 19:23:26 +0700

    first commit

---
+++ src/pages/pagesData.jsx
@@ -0,0 +1,17 @@
+import About from "./About";
+import Home from "./Home";
+
+const pagesData = [
+  {
+    path: "",
+    element: <Home />,
+    title: "home",
+  },
+  {
+    path: "about",
+    element: <About />,
+    title: "about",
+  },
+];
+
+export default pagesData;
